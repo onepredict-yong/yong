2ce4a959ebcfa0c1df16d09faed45262e062413a
yong <yongchae.kim@onepredict.com>
2023-06-27 17:29:38 +0900

    yyy

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-print("aaaa")
-print("b")
-print("c")
+import numpy as np
+
+class calculator():
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def func_plus(self):
+        self.result = self.a + self.b
+        return self.result
+    
+    def func_minus(self):
+        self.result = self.a - self.b
+    
+    def func_multiple(self):
+        self.result = self.a * self.b
+
+    def func_divide(self):
+        if self.b == 0:
+            print("Cannot calculate !")
+        else:
+            self.result = self.a / self.b
+            return self.result
+
+if __name__:
+    a = 1
+    b = 2
+    c = calculator(a, b).func_divide()
+    print(c)
